deec4c434fe6ab3b0678773cea9612d58f0ad5d8
MrPythonDeveloper <youngprogrammer1997@gmail.com>
2020-06-16 14:54:24 +0430

    add blog app

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'blogpy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogpydb',
+        'USER' : 'postgres',
+        'PASSWORD' : 'saiedabdi5294074',
+        'HOST' : 'localhost',
+        'PORT':'5432',
     }
 }
 
