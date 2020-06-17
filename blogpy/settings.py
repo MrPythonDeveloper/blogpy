c7449bdb556ff108d1ce871ed8547871a090bc36
MrPythonDeveloper <youngprogrammer1997@gmail.com>
2020-06-17 10:12:48 +0430

    add index html to blog app

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -127,8 +127,11 @@ USE_TZ = True
 #CDEditor settings
 #Static Settings
 
-STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #CKEditor
 
