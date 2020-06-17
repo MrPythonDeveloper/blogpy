7b8de17fc6e2f37be5446c88237f81cc250b9265
MrPythonDeveloper <youngprogrammer1997@gmail.com>
2020-06-17 14:08:46 +0430

    added slider and contact and about page

--- blog/views.py
+++ blog/views.py
@@ -13,11 +13,31 @@ class IndexPage(TemplateView):
             article_data.append({
                 'title' : article.title,
                 'cover' : article.cover.url,
-                'category' : article.created_at.date(),
+                'category' : article.category.title,
+                'created_at' : article.created_at.date(),
+            })
+        promote_data = []
+        all_promote_articles = Article.objects.filter(promote=True)
+        for promote_article in all_promote_articles:
+            promote_data.append({
+                'category': promote_article.category.title,
+                'title' : promote_article.title,
+                'author': promote_article.author.user.first_name + ' '+  promote_article.author.user.last_name,
+                'avatar' : promote_article.author.avatar.url if promote_article.author.avatar else None,
+                'cover' : promote_article.cover.url if promote_article.cover else None,
+                'created_at' : promote_article.created_at.date()
             })
 
         context = {
             'article_data' : article_data,
+            'promote_article_data': promote_data,
         }
 
         return render (request,'index.html', context)
+
+
+class ContactPage(TemplateView):
+    template_name = "page-contact.html"
+
+class About(TemplateView):
+    template_name = "page-about.html"
